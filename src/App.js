b362e8c9bc2398897f031afb14e06bfb51b5406a
Helom Berhane <helomberhane@gmail.com>
2020-03-21 17:14:48 -0500

    gitignore fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import { Provider } from 'react-redux'
+import store from './reducers/store'
+
+import Home from './components/LandingPage/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Provider store={store}>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
-export default App;
+export default App
